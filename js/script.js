d63cc4ef334613ecf646041353bb9a6973c68956
Md Rokan Uddin <rokanuddinroman@gmail.com>
2022-02-17 13:51:53 +0600

    Functions Done

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function putInputToResult(inputId) {
     const resultInput = document.getElementById(inputId);
     // const resultText = resultInput.innerText;
     return resultInput;
-}
+};
 
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
@@ -23,4 +23,22 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const myBalance = putInputToResult('myBalance')
     const totalBalance = incomeTotal - totalExpenses;
     myBalance.innerText = totalBalance;
-})
+});
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    // previous codes 
+    const incomeTotal = getInputFromField('incomeField');
+    const foodTotal = getInputFromField('foodField')
+    const rentTotal = getInputFromField('rentField')
+    const clothesTotal = getInputFromField('clothesField')
+    const totalExpenses = foodTotal + rentTotal + clothesTotal;
+    const totalBalance = incomeTotal - totalExpenses;
+    // new codes 
+    const savePercentage = getInputFromField('saveField');
+    const mySave = putInputToResult('mySave');
+    const totalSave = incomeTotal - ((100 - savePercentage) * 100);
+    mySave.innerText = totalSave;
+    const remainingBalance = putInputToResult('remainingBalance');
+    const totalRemainingBalance = totalBalance - totalSave;
+    remainingBalance.innerText = totalRemainingBalance;
+});
